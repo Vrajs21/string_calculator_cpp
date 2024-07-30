833d595238e6e5a2e9778f20ad52e0eb2f5dbf45
Vrajs21 <vrajs2103@gmail.com>
2024-07-30 23:22:13 +0530

    Test Case 0.1 : Handle Single Interger Input

--- src/string_calculator.cpp
+++ src/string_calculator.cpp
@@ -1,14 +1,17 @@
 #include "../include/string_calculator.hpp"
-#include <sstream>
-#include <string>
-#include <vector>
-
+#include<bits/stdc++.h>
+#include <algorithm> 
+#include <cctype> 
 using namespace std;
-int StringCalculator::add(const std::string& numbers) {
+
+int StringCalculator::add(const string& numbers) {
     
     if (numbers.empty()) {
         return 0;
     }
 
+    if(numbers.length()==1 && isdigit(numbers[0]))
+        return stoi(numbers.substr(0));
+        
 
 }
--- test/string_calculator_test.cpp
+++ test/string_calculator_test.cpp
@@ -6,6 +6,9 @@ GTEST_TEST(StringCalculatorTest, EmptyString) {
     EXPECT_EQ(StringCalculator::add(""), 0);
 }
 
+GTEST_TEST(StringCalculatorTest,Check_Single_Interger_input) {
+    EXPECT_EQ(StringCalculator::add("3"), 3);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
