4fea8a70d8034b891b5a761951fcd86e4ce384cb
Vrajs21 <vrajs2103@gmail.com>
2024-07-31 00:02:37 +0530

    TEST CASE 1 : allow multiple integer input with ,

--- src/string_calculator.cpp
+++ src/string_calculator.cpp
@@ -12,6 +12,26 @@ int StringCalculator::add(const string& numbers) {
 
     if(numbers.length()==1 && isdigit(numbers[0]))
         return stoi(numbers.substr(0));
-        
+    
+   
 
+    int sum = 0;
+    int num = 0;
+    bool negative = false;
+
+    for (char c : numbers) {
+        if (c == '-') {
+            negative = true;
+        } else if (c == ',') {
+            sum += negative ? -num : num;
+            num = 0;
+            negative = false;
+        } else {
+            num = num * 10 + (c - '0');
+        }
+    }
+
+    sum += negative ? -num : num;
+
+    return sum;
 }
--- test/string_calculator_test.cpp
+++ test/string_calculator_test.cpp
@@ -10,6 +10,22 @@ GTEST_TEST(StringCalculatorTest,Check_Single_Interger_input) {
     EXPECT_EQ(StringCalculator::add("3"), 3);
 }
 
+GTEST_TEST(StringCalculatorTest, TwoNumbers) {
+    EXPECT_EQ(6, StringCalculator::add("1,5"));
+}
+
+GTEST_TEST(StringCalculatorTest, MultipleNumbers) {
+    EXPECT_EQ(15, StringCalculator::add("1,2,3,4,5"));
+}
+
+GTEST_TEST(StringCalculatorTest, NegativeNumbers) {
+    EXPECT_EQ(-5, StringCalculator::add("-1,-4"));
+}
+
+GTEST_TEST(StringCalculatorTest, MixedNumbers) {
+    EXPECT_EQ(0, StringCalculator::add("1,-1"));
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
